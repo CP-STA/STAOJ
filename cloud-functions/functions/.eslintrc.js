178d9459748840eb274bfe316a87b8f1480b01ec
Deyao Chen <chendeyao000@gmail.com>
2022-08-24 17:00:26 +0800

    at least there isn't any error

--- cloud-functions/functions/.eslintrc.js
+++ cloud-functions/functions/.eslintrc.js
@@ -1,12 +1,16 @@
 module.exports = {
-  root: true,
-  env: {
+  "root": true,
+  "env": {
     es6: true,
     node: true,
   },
-  extends: ["eslint:recommended", "google"],
-  rules: {
-    quotes: ["error", "double"],
-    indent: ["error", 2],
+  "extends": ["eslint:recommended", "google"],
+  "rules": {
+    "quotes": ["error", "double"],
+    "indent": ["error", 2],
+    "max-len": "off",
+  },
+  "parserOptions": {
+    "ecmaVersion": 2017,
   },
 };
--- cloud-functions/functions/index.js
+++ cloud-functions/functions/index.js
@@ -4,14 +4,35 @@ const fetch = require("node-fetch");
 
 admin.initializeApp();
 
+/**
+ * This gets the problem statement from the github repo.
+ * @param {string} problem
+ * @return {Promise<any>}
+ */
+function getProblemStatement(problem) { // eslint-disable-line no-unused-vars
+  return fetch(
+    `https://raw.githubusercontent.com/CP-STA/contest-problems/main/${problem}/statement.json`,
+  )
+    .then((res) => res.json());
+}
+
+/**
+ * Gets the test case from the github repo.
+ * @param {string} problem
+ * @return {Promise<any>}
+ */
+function getTestCases(problem) { // eslint-disable-line no-unused-vars
+  return fetch(
+    `https://raw.githubusercontent.com/CP-STA/contest-problems/main/${problem}/test-cases.json`,
+  )
+    .then((res) => res.json());
+}
+
 exports.writeSubtasks = functions.firestore
   .document("/submissions/{submissionId}")
-  .onCreate((snap, context) => {
+  .onCreate(async (snap, context) => {
     const problem = snap.data().problem;
-    return fetch(
-      `https://raw.githubusercontent.com/CP-STA/contest-problems/main/${problem}/statement.json`
-    )
-      .then((res) => res.json())
+    return getProblemStatement(problem)
       .then((json) => {
         let subtasks = {};
         subtasks = {};
@@ -21,10 +42,24 @@ exports.writeSubtasks = functions.firestore
         return snap.ref.set({subtasks}, {merge: true});
       });
   });
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+
+exports.parseJudge = functions.firestore
+  .document("/submissions/{submissionId}/judge-result/{judgeResultId}")
+  .onCreate(async (snap, context) => {
+    const db = admin.firestore();
+    const submissionRef = db.collection("submissions").doc(context.params.submissionId);
+    const submissionSnap = await submissionRef.get();
+    const problem = submissionSnap.data().problem;
+    const testCasesPromise = getTestCases(problem);
+    const testCases = await testCasesPromise;
+    if (snap.data().state == "tested" && snap.data().result != "accepted") {
+      const currentTestCase = snap.data().testCase;
+      const currentSubtask = testCases[currentTestCase-1];
+      const key = `subtasks.${currentSubtask}`;
+      const obj = {};
+      obj[key] = false;
+      return submissionRef.update(obj);
+    } else {
+      // TODO: handle normal submission
+    }
+  });
