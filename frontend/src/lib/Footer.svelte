f19a2f8eb2bfaf170e76ccd282b16504920f35c6
Deyao Chen <chendeyao000@gmail.com>
2022-08-31 12:21:58 +0800

    add banner for submission results

--- frontend/src/lib/Footer.svelte
+++ frontend/src/lib/Footer.svelte
@@ -1,7 +1,7 @@
   <footer class="container-fluid mt-auto pt-3">
     <div class="border-top">
       <div class="container d-flex justify-content-between mt-3">
-      <p>&copy; STAOJ contributors. Proudly FOS under MIT.</p>
+      <p>&copy; STAOJ contributors. Proudly FOS under the MIT license.</p>
       <ul class="list-unstyled d-flex">
         <li class="ms-3"><a href="https://github.com/cp-sta/staoj" class="link-dark"><i class="bi bi-github"></i></a></li>
       </ul>
