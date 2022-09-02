e878d008657c7cf825a0c1c576104032d82af777
Deyao Chen <chendeyao000@gmail.com>
2022-09-02 12:39:50 +0800

    add conditional testing depending on the command line arg, it either reads from key.json or key-testing.json to connect to different database

--- executioner/start.mjs
+++ executioner/start.mjs
@@ -2,4 +2,10 @@ import * as dotenv from 'dotenv';
 import { start } from './src/app.mjs';
 
 dotenv.config();
+
+if (process.argv.includes('--testing')) {
+  process.env.GOOGLE_APPLICATION_CREDENTIALS = process.env.GOOGLE_APPLICATION_CREDENTIALS_TESTING
+}
+
 start();
+
