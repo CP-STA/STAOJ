58c4e7b2abc06fa0f7223b746021a00b17a2c0b0
Deyao Chen <chendeyao000@gmail.com>
2022-09-02 12:32:52 +0800

    read key files conditionally

--- executioner/start.mjs
+++ executioner/start.mjs
@@ -2,4 +2,12 @@ import * as dotenv from 'dotenv';
 import { start } from './src/app.mjs';
 
 dotenv.config();
+
+if (process.argv.includes('--testing')) {
+  process.env.GOOGLE_APPLICATION_CREDENTIALS = process.env.GOOGLE_APPLICATION_CREDENTIALS_TESTING
+}
+
+console.log(process.env.GOOGLE_APPLICATION_CREDENTIALS)
+
 start();
+
