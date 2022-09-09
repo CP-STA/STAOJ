ff5d91bd500337b9f6de771c5cd92f78725f4438
Deyao Chen <chendeyao000@gmail.com>
2022-09-09 01:19:49 +0100

    update feed-test

--- tools/feed-test.py
+++ tools/feed-test.py
@@ -9,9 +9,8 @@ with open('test-cases.json') as f:
 for i, test_case in enumerate(test_cases):
   p = subprocess.run(sys.argv[1:], input=test_case['input'].encode('utf-8'), capture_output=True)
   answer = p.stdout.decode('utf-8')
-  answer = int(answer)
-  if answer != int(test_case['output']):
-    print(i, answer, int(test_case['output']))
+  if answer.split() != test_case['output'].split():
+    print(i, 'incorrect')
   else:
     print(i, 'correct')
 
