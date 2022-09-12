918548b3e58e8adeca2c570b2bc446a1c979b8ee
Deyao Chen <chendeyao000@gmail.com>
2022-09-12 15:20:21 +0100

    standing tallys completed

--- frontend/src/routes/account/+page.svelte
+++ frontend/src/routes/account/+page.svelte
@@ -1,10 +1,12 @@
 <script>
 	// @ts-nocheck
 
-	import { app as firebaseApp } from '$lib/firebase';
+	import { app as firebaseApp, db } from '$lib/firebase';
 	import { getAuth, signOut, signInWithPopup, GithubAuthProvider } from 'firebase/auth';
 	import { user } from '$lib/User.svelte';
 	import { formatTitle } from '$lib/utils';
+	import { onSnapshot, doc, setDoc } from 'firebase/firestore';
+	import { onDestroy } from 'svelte';
 
 	const provider = new GithubAuthProvider();
 	const auth = getAuth(firebaseApp);
@@ -18,6 +20,53 @@
 		signOut(auth).catch((error) => {
 			console.error(error);
 		});
+	}
+
+	let displayName = 'Loading...';
+	let isNameLoaded = false;
+
+	let unsub;
+
+	onDestroy(() => {
+		if (unsub) {
+			unsub();
+		}
+	});
+
+	$: if (!isNameLoaded && $user.loaded && $user.user) {
+		if (unsub) {
+			unsub();
+		}
+		unsub = onSnapshot(doc(db, 'users', $user.user.uid), (doc) => {
+			if (doc.data()) {
+				displayName = doc.data().displayName;
+			} else {
+				displayName = $user.user.displayName;
+			}
+			isNameLoaded = true;
+		});
+	}
+
+	let nothingState = Symbol();
+	let errorState = Symbol();
+	let waitingState = Symbol();
+	let successState = Symbol();
+
+	let updateState = nothingState;
+
+	async function updateInfo() {
+		if ($user.loaded && $user.user) {
+			updateState = waitingState;
+			try {
+				await setDoc(doc(db, 'users', $user.user.uid), {
+					displayName
+				});
+			} catch (e) {
+				updateState = errorState;
+				throw e;
+			}
+			updateState = successState;
+		}
 	}
 </script>
 
@@ -34,7 +83,22 @@
 {#if !$user.loaded}
 	<p>Loading...</p>
 {:else if $user.user}
-	<p>Hello {$user.user.displayName}.</p>
+	<form on:submit|preventDefault={updateInfo}>
+		<div class="form-group">
+			<label for="displayName">Name</label>
+			<input bind:value={displayName} type="text" class="form-control" id="DisplayName" />
+			{#if updateState == successState}
+				<div class="alert alert-success mt-3" role="alert">Update Successful</div>
+			{:else if updateState == errorState}
+				<div class="alert alert-danger mt-3" role="alert">Update Unsuccessful</div>
+			{:else if updateState == waitingState}
+				<div class="alert alert-warning mt-3" role="alert">Updating</div>
+			{/if}
+			<button type="submit" class="btn btn-primary" class:mt-3={updateState == nothingState}
+				>Update</button
+			>
+		</div>
+	</form>
 {:else}
 	<button type="button" class="w-100 btn btn-lg btn-primary mb-3" on:click={signInWithGithub}
 		><i class="bi bi-github" /> Sign in with Github</button
--- tools/standings-tally/main.ts
+++ tools/standings-tally/main.ts
@@ -3,9 +3,19 @@ dotenv.config();
 
 import { initializeApp } from 'firebase-admin/app';
 import { getFirestore } from 'firebase-admin/firestore';
-import { getDocs, query, where, collection } from 'firebase/firestore';
+import { getAuth } from 'firebase-admin/auth';
 import * as readline from 'node:readline';
 import { stdin, stdout } from 'node:process';
+
+
+async function getDocs(docs: FirebaseFirestore.QuerySnapshot<FirebaseFirestore.DocumentData>): Promise<FirebaseFirestore.QueryDocumentSnapshot<FirebaseFirestore.DocumentData>[]> {
+  return await new Promise((resolve) => {
+    let docsList: FirebaseFirestore.QueryDocumentSnapshot<FirebaseFirestore.DocumentData>[] = []
+    docs.forEach((doc) => {
+      docsList.push(doc);
+      if (docsList.length == docs.size) {resolve(docsList)}
+    })})
+}
 
 async function main() {
   const app = initializeApp();
@@ -15,59 +25,106 @@ async function main() {
   const contestName: string = await new Promise(resolve => rl.question("Contest name: ", (answer) => {resolve(answer)}));
   
   const contestDefinitionPath = `../../problems-private/contests/${contestName}.json`;
-  const contestDefinition = require(contestDefinitionPath);
+  const contestDefinition: {info: any, problems: {slug: string, name: string}[]} = require(contestDefinitionPath);
   const submissionCollection = db.collection('submissions'); 
   interface UserStanding {
     [uid: string]: {
-      scores: {[problem: string]: number},
+      displayName: string,
+      problems: {
+        [problem: string]: {
+          score: number
+        }
+      }
       total: number
     };
   }
-  
-  
+
   let people: UserStanding = {};
   let success = false;
   let problems = contestDefinition.problems.map((x: any) => {
     return x.slug
   });
+
+  let problemsOrder: string[] = [];
+  let problemsNames: {[problem: string]: string} = {}
+  for (const problem of contestDefinition.problems) {
+    problemsOrder.push(problem.slug);
+    problemsNames[problem.slug] = problem.name;
+  }
+
   while (!success) {
     const docs = await submissionCollection.where('submissionTime', '>=', new Date(contestDefinition.info.startTime)).where('submissionTime', '<=', new Date(contestDefinition.info.endTime)).get();
     people = {};
     success = true; 
-    docs.forEach((doc) => {
+    const docsList = await getDocs(docs); 
+    for (const doc of docsList){ try {
       if (!success) {return};
       const data = doc.data();
       if (data.state != "judged") {
         success = false;
         return
       } 
+
       if (!(data.user in people)) {
-        let scores: {[problem: string]: number} = {};
+        let problems: {[problem: string]: {score: number}} = {};
         for (const problem of contestDefinition.problems) {
-          scores[problem.slug] = 0; 
+          problems[problem.slug] = {
+            score: 0
+          }
+        }
+        let displayName: string = data.user;
+        const a = db.collection("users").doc(data.user);
+        const userInfo: {displayName: string} | null = await new Promise(
+          (resolve) => {
+            a.get().then((doc) => {if (doc.exists) {resolve(doc.data() as {displayName: string})} else {resolve(null)}}).catch((err) => {console.error(err); resolve(null)})
+          });
+        if (userInfo) {
+          displayName = userInfo.displayName
+        } else {
+          try {
+            const userRecord = await getAuth().getUser(data.user);
+            if (userRecord.displayName) {displayName = userRecord.displayName}
+          } catch (e) {
+            console.error(e);
+          }
         }
         people[data.user] = {
-          scores,
+          displayName,
+          problems,
           total: 0
         }
       }
-      console.log(data.score, people[data.user as string].scores[data.problem])
-      if (data.score > people[data.user as string].scores[data.problem]) {
-        const oldScore = people[data.user as string].scores[data.problem];
-        people[data.user].scores[data.problem as string] = data.score;
+      if (data.score > people[data.user as string].problems[data.problem as string].score) {
+        const oldScore = people[data.user as string].problems[data.problem as string].score;
+        people[data.user].problems[data.problem as string].score = data.score;
         people[data.user].total += (data.score - oldScore)
       }
-    })
+    } catch (e) {console.error(e)} }
     if (!success) {
       console.log("Some submissions have not finished judging, retrying in 1 second");
       await new Promise(resolve => setTimeout(resolve, 1000))
     }
   }
 
-  await db.collection("standings").doc(contestName).set(people);
+  let usersOrder: { uid: string; data: { total: number; }; }[] = [];
+  for (const [uid, data] of Object.entries(people)) {
+    usersOrder.push({uid, data});
+  }
+
+  usersOrder.sort((first, second) => {
+    return - first.data.total + second.data.total; 
+  })
+
+  await db.collection("standings").doc(contestName).set({
+    name: contestDefinition.info.name,
+    startTime: new Date(contestDefinition.info.startTime), 
+    users: people,
+    usersOrder: usersOrder.map(x => x.uid),
+    problemsOrder,
+    problemsNames
+  });
+
   console.log(people);
-
-
   process.exit(0);
 }
 
