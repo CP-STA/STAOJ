6ad5a7166ea3e46624b49abb0b745dbe5342c77d
Deyao Chen <chendeyao000@gmail.com>
2022-09-12 15:22:06 +0100

    update feed-test

--- tools/feed_test.py
+++ tools/feed_test.py
@@ -10,7 +10,7 @@ for i, test_case in enumerate(test_cases):
   p = subprocess.run(sys.argv[1:], input=test_case['input'].encode('utf-8'), capture_output=True)
   answer = p.stdout.decode('utf-8')
   if answer.split() != test_case['output'].split():
-    print(i, 'incorrect')
+    print(i+1, 'incorrect')
   else:
-    print(i, 'correct')
+    print(i+1, 'correct')
 
