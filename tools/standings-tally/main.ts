ac7705342481d81e8edeb4f1c997113875682657
Deyao Chen <chendeyao000@gmail.com>
2022-09-12 11:15:48 +0100

    working tally script

--- tools/standings-tally/main.ts
+++ tools/standings-tally/main.ts
@@ -3,9 +3,19 @@ dotenv.config();
 
 import { initializeApp } from 'firebase-admin/app';
 import { getFirestore } from 'firebase-admin/firestore';
-import { getDocs, query, where, collection } from 'firebase/firestore';
+import { getAuth } from 'firebase-admin/auth';
 import * as readline from 'node:readline';
 import { stdin, stdout } from 'node:process';
+
+
+async function getDocs(docs: FirebaseFirestore.QuerySnapshot<FirebaseFirestore.DocumentData>): Promise<FirebaseFirestore.QueryDocumentSnapshot<FirebaseFirestore.DocumentData>[]> {
+  return await new Promise((resolve) => {
+    let docsList: FirebaseFirestore.QueryDocumentSnapshot<FirebaseFirestore.DocumentData>[] = []
+    docs.forEach((doc) => {
+      docsList.push(doc);
+      if (docsList.length == docs.size) {resolve(docsList)}
+    })})
+}
 
 async function main() {
   const app = initializeApp();
@@ -19,12 +29,12 @@ async function main() {
   const submissionCollection = db.collection('submissions'); 
   interface UserStanding {
     [uid: string]: {
+      displayName: string,
       scores: {[problem: string]: number},
       total: number
     };
   }
-  
-  
+
   let people: UserStanding = {};
   let success = false;
   let problems = contestDefinition.problems.map((x: any) => {
@@ -34,40 +44,66 @@ async function main() {
     const docs = await submissionCollection.where('submissionTime', '>=', new Date(contestDefinition.info.startTime)).where('submissionTime', '<=', new Date(contestDefinition.info.endTime)).get();
     people = {};
     success = true; 
-    docs.forEach((doc) => {
+    const docsList = await getDocs(docs); 
+    for (const doc of docsList){ try {
       if (!success) {return};
       const data = doc.data();
       if (data.state != "judged") {
         success = false;
         return
       } 
+
       if (!(data.user in people)) {
         let scores: {[problem: string]: number} = {};
         for (const problem of contestDefinition.problems) {
           scores[problem.slug] = 0; 
         }
+        let displayName: string = data.user;
+        const a = db.collection("users").doc(data.user);
+        const userInfo: {displayName: string} | null = await new Promise(
+          (resolve) => {
+            a.get().then((doc) => {if (doc.exists) {resolve(doc.data() as {displayName: string})} else {resolve(null)}}).catch((err) => {console.error(err); resolve(null)})
+          });
+        if (userInfo) {
+          displayName = userInfo.displayName
+        } else {
+          try {
+            const userRecord = await getAuth().getUser(data.user);
+            if (userRecord.displayName) {displayName = userRecord.displayName}
+          } catch (e) {
+            console.error(e);
+          }
+        }
         people[data.user] = {
+          displayName,
           scores,
           total: 0
         }
       }
-      console.log(data.score, people[data.user as string].scores[data.problem])
       if (data.score > people[data.user as string].scores[data.problem]) {
         const oldScore = people[data.user as string].scores[data.problem];
         people[data.user].scores[data.problem as string] = data.score;
         people[data.user].total += (data.score - oldScore)
       }
-    })
+    } catch (e) {console.error(e)} }
     if (!success) {
       console.log("Some submissions have not finished judging, retrying in 1 second");
       await new Promise(resolve => setTimeout(resolve, 1000))
     }
   }
 
-  await db.collection("standings").doc(contestName).set(people);
+  await db.collection("standings").doc(contestName).set({
+    name: contestDefinition.info.name,
+    startTime: new Date(contestDefinition.info.startTime), 
+    users: people
+  });
+  
+  await db.collection("standings").doc("alpha-2").set({
+    name: contestDefinition.info.name,
+    startTime: new Date(contestDefinition.info.startTime), 
+    users: people
+  });
   console.log(people);
-
-
   process.exit(0);
 }
 
