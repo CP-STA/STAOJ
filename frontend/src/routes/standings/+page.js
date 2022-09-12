4c8188a499b4c6343e2927163adbba94fc79b4cf
Deyao Chen <chendeyao000@gmail.com>
2022-09-12 15:27:47 +0100

    bug fix

--- frontend/src/routes/standings/+page.js
+++ frontend/src/routes/standings/+page.js
@@ -4,15 +4,15 @@ import { error } from '@sveltejs/kit';
 
 /** @type {import('./$types').PageLoad} */
 export async function load({ params, fetch, url }) {
+  console.log("hello");
 	const docs = await getDocs(query(collection(db, 'standings'), orderBy('startTime', 'desc')));
 
-	let standingsData;
-	if (docs.size == 0) {
-		standingsData = [];
-	}
-	standingsData = await new Promise((resolve) => {
+	let standingsData = await new Promise((resolve) => {
 		/** @type {any[]} */
 		let newStandingsData = [];
+    if (docs.size == 0) {
+      resolve(newStandingsData);
+    }
 		docs.forEach((doc) => {
 			newStandingsData.push(doc.data());
 			if (newStandingsData.length == docs.size) {
