57ff80654b5e9d2996732e4e9475ba55f97f3b03
Deyao Chen <chendeyao000@gmail.com>
2022-09-28 19:20:02 +0100

    bug fix

--- tools/time-server/main.ts
+++ tools/time-server/main.ts
@@ -3,4 +3,4 @@ import http from 'http';
 http.createServer(function (req, res) {
     res.write((new Date()).toISOString()); //write a response to the client
     res.end(); //end the response
-  }).listen('/workspaces/STAOJ/tools/time-server/http.sock'); //the server object listens on port 8080 
+  }).listen('http.sock'); //the server object listens on port 8080 
