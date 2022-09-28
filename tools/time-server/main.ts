d9b633fa17e17c2215f7b3cb671b2bef5f7373ec
Deyao Chen <chendeyao000@gmail.com>
2022-09-28 19:29:10 +0100

    use TCP port instead of unix socket for time-server

--- tools/time-server/main.ts
+++ tools/time-server/main.ts
@@ -3,4 +3,4 @@ import http from 'http';
 http.createServer(function (req, res) {
     res.write((new Date()).toISOString()); //write a response to the client
     res.end(); //end the response
-  }).listen('http.sock'); //the server object listens on port 8080 
+  }).listen(5433); //the server object listens on port 8080 
