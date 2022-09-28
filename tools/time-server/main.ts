c5eb4897323ded69c66063ce74a970b5291d0b33
Deyao Chen <chendeyao000@gmail.com>
2022-09-28 19:56:04 +0100

    fix cors for time-server

--- tools/time-server/main.ts
+++ tools/time-server/main.ts
@@ -1,6 +1,10 @@
 import http from 'http';
 
 http.createServer(function (req, res) {
+    const headers = {
+      'Access-Control-Allow-Origin': '*', //just a time server, nothing to worry about
+    };
+    res.writeHead(200, headers);
     res.write((new Date()).toISOString()); //write a response to the client
     res.end(); //end the response
   }).listen(5433); //the server object listens on port 8080 
