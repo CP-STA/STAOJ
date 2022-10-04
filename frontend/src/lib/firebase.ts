91e90d3c3e12886800d89f04355186cc21ecb69a
Deyao Chen <chendeyao000@gmail.com>
2022-10-04 12:55:20 +0100

    run format

--- frontend/src/lib/firebase.ts
+++ frontend/src/lib/firebase.ts
@@ -6,12 +6,12 @@ import { browser } from '$app/env';
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAx7IuEW24_yQDmJZ_h1U0VOkZbxXWz1ho",
-  authDomain: "staoj-backend-testing.firebaseapp.com",
-  projectId: "staoj-backend-testing",
-  storageBucket: "staoj-backend-testing.appspot.com",
-  messagingSenderId: "814273570261",
-  appId: "1:814273570261:web:595a3129fe93ac3682c95c"
+	apiKey: 'AIzaSyAx7IuEW24_yQDmJZ_h1U0VOkZbxXWz1ho',
+	authDomain: 'staoj-backend-testing.firebaseapp.com',
+	projectId: 'staoj-backend-testing',
+	storageBucket: 'staoj-backend-testing.appspot.com',
+	messagingSenderId: '814273570261',
+	appId: '1:814273570261:web:595a3129fe93ac3682c95c'
 };
 
 // Initialize Firebase
