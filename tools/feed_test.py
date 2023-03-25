b348c8b07395afef1b6532ff2b15a049bd527e51
Deyao Chen <chendeyao000@gmail.com>
2023-03-25 17:40:36 +0000

    improve feed_test.py

--- tools/feed_test.py
+++ tools/feed_test.py
@@ -4,6 +4,11 @@ import subprocess
 import json
 import time
 
+parser = argparse.ArgumentParser()
+parser.add_argument('--no-display-wrong', '--nd', action='store_true')
+parser.add_argument('cmd', nargs='+')
+args = parser.parse_args()
+
 class bcolors:
     HEADER = '\033[95m'
     OKBLUE = '\033[94m'
@@ -21,10 +26,18 @@ with open('test-cases.json') as f:
 has_incorrect = False
 for i, test_case in enumerate(test_cases):
   start = time.time()
-  p = subprocess.run(sys.argv[1:], input=test_case['input'].encode('utf-8'), capture_output=True)
+  p = subprocess.run(args.cmd, input=test_case['input'].encode('utf-8'), capture_output=True)
   answer = p.stdout.decode('utf-8')
   if answer.split() != test_case['output'].split():
-    print(i+1, bcolors.FAIL + 'incorrect' + bcolors.ENDC)
+    if 'subtask' in test_case:
+      print(i+1, bcolors.FAIL + f'incorrect for subtask {test_case["subtask"]}' + bcolors.ENDC)
+    else:
+      print(i+1, bcolors.FAIL + 'incorrect' + bcolors.ENDC)
+    if not args.no_display_wrong:
+      print("input was ", test_case['input'])
+      print("should be ", test_case['output'].split())
+      print("but found ", answer.split())
+      print("stderr was ", p.stderr.decode('utf-8'))
     has_incorrect = True
   else:
     print(i+1, 'correct')
