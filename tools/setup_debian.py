857ace5569f2388293b45d25598cc1d2996e3fd2
Deyao Chen <chendeyao000@gmail.com>
2024-03-09 14:50:50 +0000

    update setup script

--- tools/setup_debian.py
+++ tools/setup_debian.py
@@ -10,11 +10,17 @@ ssh_host = args.ssh_host
 def run(s):
     subprocess.run(['ssh', '-t', ssh_host, s], check=True)
 
+run('curl -fsSL https://deb.nodesource.com/setup_21.x | sudo bash -')
+run('sudo apt-get install -y nodejs')
 run('sudo apt-get update')
-run('sudo apt-get -y install rsync podman slirp4netns fuse-overlayfs git nodejs npm')
+run('sudo apt-get -y install aptitude')
+run('sudo aptitude install -y npm')
+run('sudo apt-get -y install rsync podman slirp4netns fuse-overlayfs git')
 run('sudo usermod --add-subuids 100000-165535 --add-subgids 100000-165535 $(whoami)')
 run('git clone https://github.com/CP-STA/STAOJ || true')
 run('cd STAOJ && git submodule update --init')
+run('cd STAOJ/problems-private && git pull')
+run('cd STAOJ/problems && git pull')
 run('cd STAOJ/executioner && npm install')
 
 key = os.path.join(os.path.dirname(os.path.dirname(__file__)), 'executioner', 'key.json')
