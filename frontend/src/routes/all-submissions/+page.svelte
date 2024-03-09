1e006fb38ba9b696df0502b73e4fe0aed4141a16
Deyao Chen <chendeyao000@gmail.com>
2024-03-09 16:07:43 +0000

    remove not needed import

--- frontend/src/routes/all-submissions/+page.svelte
+++ frontend/src/routes/all-submissions/+page.svelte
@@ -1,6 +1,5 @@
 <script>
 	import PreviousSubmissions from '$lib/PreviousSubmissions.svelte';
-	import User from '$lib/User.svelte';
 	import { db } from '$lib/firebase';
 	import { query, collection, orderBy, limit, where } from 'firebase/firestore';
 	const q = query(collection(db, 'submissions'), orderBy('submissionTime', 'desc'), limit(50));
